1368555dce67dc59a2a0d01160bf4f5a8164df3b
Lefford <lefford.girigori@gmail.com>
2012-03-15 11:55:57 +0100

    Replaced flatpages with own implementation, contentblock and removed admin code

--- post/models.py
+++ post/models.py
@@ -1,23 +1,65 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.flatpages.models import FlatPage
+from django import forms
+from django.contrib.sites.models import Site
 
-# Create your models here.
-
-class Post(models.Model):
-	title 		= models.CharField(max_length=255)
-	date_created 	= models.DateField(auto_now_add=True)
-	body 		= models.TextField()
+class BasePage(models.Model):
+	url 			= models.CharField(max_length=100, db_index=True)
+	title 			= models.CharField(max_length=255, blank=True)
+	template_name 		= models.CharField(max_length=70, blank=True)
+	registration_required 	= models.BooleanField()
+	#breadcrumb 		= models.CharField(max_length=20, blank=True)	
 
 	def __unicode__(self):
-		return self.title
-
-class ContentBlock(models.Model):
-	title = models.CharField(max_length=255)
+		return '{0} -- {1}'.format(self.url, self.title)
 	
 	class Meta:
 		abstract = True
 
-class PostAdmin(admin.ModelAdmin):
-	search_fields = ['title']
+class BaseContentBlock(models.Model):
+	title 			= models.CharField(max_length=30)
+	content 		= models.TextField()
 
-admin.site.register(Post, PostAdmin)
+	class Meta:
+		abstract = True
+
+class ContentBlock(BaseContentBlock):
+	
+	show_after 		= models.ForeignKey('ContentBlock', blank=True, null=True, related_name='content_predecessor')
+
+	def __unicode__(self):
+		return '{0} - {1}'.format(self.id, self.title)
+
+class ParentPage(BasePage):
+	"""
+	Parent page 
+	"""
+
+	show_after 		= models.ForeignKey('ParentPage', blank=True, null=True, related_name='page_predecessor')
+	sites 			= models.ManyToManyField(Site)
+
+	def __unicode__(self):
+		return self.title
+	
+class ChildPage(BasePage):
+	"""
+	Child page
+	"""
+
+	parent_page 		= models.ForeignKey('ParentPage')
+
+	def __unicode__(self):
+		return self.title
+
+class Post(models.Model):
+	"""
+	Entry
+	"""
+
+	title 			= models.CharField(max_length=255)
+	date_created 		= models.DateField(auto_now_add=True)
+	body 			= models.TextField()
+
+	def __unicode__(self):
+		return self.title
